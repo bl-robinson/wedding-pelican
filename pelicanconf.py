c96bf2bd4076e7b9f42c02e16d975efdd810c821
Ben Robinson <roaming.b.robinson@gmail.com>
2019-09-30 18:43:40 +0100

    Strip down for our use

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,40 +3,31 @@
 from __future__ import unicode_literals
 import os
 
-AUTHOR = 'Claudio Walser'
-SITENAME = 'FH5CO Marble Example'
+AUTHOR = 'Ben Robinson'
+SITENAME = 'Leah and Ben '
 SITEDESCRIPTION = 'this is just an example page for the pelican-fh5co-marble theme.'
-SITEURL = 'http://localhost:8081'
+# SITEURL = 'http://localhost:8081'
 
 # plugins
 PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['i18n_subsites', 'tipue_search']
+PLUGINS = ['i18n_subsites']
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
 # theme and theme localization
-THEME = '../pelican-fh5co-marble'
-I18N_GETTEXT_LOCALEDIR = '../pelican-fh5co-marble/locale/'
-I18N_GETTEXT_DOMAIN = 'messages'
-I18N_GETTEXT_NEWSTYLE = True
-TIMEZONE = 'Europe/Zurich'
-DEFAULT_DATE_FORMAT = '%a, %d %b %Y'
-I18N_TEMPLATES_LANG = 'en_US'
+THEME = '../wedding-theme'
+# I18N_GETTEXT_LOCALEDIR = '../pelican-fh5co-marble/locale/'
+# I18N_GETTEXT_DOMAIN = 'messages'
+# I18N_GETTEXT_NEWSTYLE = True
+TIMEZONE = 'Europe/London'
+# DEFAULT_DATE_FORMAT = '%a, %d %b %Y'
+# I18N_TEMPLATES_LANG = 'en_US'
 DEFAULT_LANG = 'en'
 LOCALE = 'en_US'
 
 # content paths
 PATH = 'content'
-PAGE_PATHS = ['pages/en']
-ARTICLE_PATHS = ['blog/en']
-
-# i18n
-I18N_SUBSITES = {
-  'de': {
-    'PAGE_PATHS': ['pages/de'],
-    'ARTICLE_PATHS': ['blog/de'],
-    'LOCALE': 'de_DE'
-  }
-}
+PAGE_PATHS = ['pages']
+ARTICLE_PATHS = ['blog']
 
 # logo path, needs to be stored in PATH Setting
 LOGO = '/images/logo.svg'
@@ -48,11 +39,9 @@ HERO = [
     # for multilanguage support, create a simple dict
     'title': {
       'en':'Some special content',
-      'de': 'Spezieller Inhalt'
     },
     'text': {
       'en': 'Any special content you want to tease here',
-      'de': 'Jeglicher spezieller Inhalt den Sie hier bewerben möchten'
     },
     'links': [
       {
@@ -81,21 +70,12 @@ HERO = [
   }
 ]
 
-# Social widget
-SOCIAL = (
-  ('Github', 'https://www.github.com/claudio-walser'),
-  ('Facebook', 'https://www.facebook.com'),
-  ('Twitter', 'https://www.twitter.com'),
-  ('Google+', 'https://plus.google.com')
-)
-
 ABOUT = {
   'image': '/images/about/about.jpeg',
   'mail': 'info@gitcd.io',
   # keep it a string if you dont need multiple languages
   'text': {
     'en': 'Learn more about the creator of this theme or just drop a message.',
-    'de': 'Lernen Sie den Author kennen oder hinterlassen Sie einfach eine Nachricht'
   },
   'link': 'contact.html',
   # the address is also taken for google maps
@@ -109,26 +89,7 @@ DISPLAY_PAGES_ON_HOME = True
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_TAGS_ON_MENU = False
 USE_FOLDER_AS_CATEGORY = True
-PAGE_ORDER_BY = 'order'
-
-MENUITEMS = [
-  ('Archive', 'archives.html'),
-  ('Contact', 'contact.html')
-]
-
-DIRECT_TEMPLATES = [
-  'index',
-  'tags',
-  'categories',
-  'authors',
-  'archives',
-  'search', # needed for tipue_search plugin
-  'contact' # needed for the contact form
-]
-
-# setup disqus
-DISQUS_SHORTNAME = 'gitcd-dev'
-DISQUS_ON_PAGES = False # if true its just displayed on every static page, like this you can still enable it per page
+# PAGE_ORDER_BY = 'order'
 
 # setup google maps
 GOOGLE_MAPS_KEY = 'AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA'
